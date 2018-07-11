0e2426813b13a0b8d335f64716e36ec8b33848e7
Jonas Luebbers <jonaskluebbers@gmail.com>
2018-07-10 23:44:16 -0400

    Wait to load image until script runs

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const image = document.querySelector('.image-container img')
+image.src = image.dataset.src
 image.addEventListener('load', () => {
     image.parentElement.classList.add('is-loaded')
     image.parentElement.style.backgroundImage = `url('${image.src}')`
