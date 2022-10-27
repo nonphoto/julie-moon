6c4635abdd7180598e48dd065d163b487cb799c7
Jonas Luebbers <jonas@jonasluebbers.com>
2022-10-27 11:56:07 -0700

    Add image animation and footer

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-const image = document.querySelector('.image-container img')
-image.src = image.dataset.src
-image.addEventListener('load', () => {
-    image.parentElement.classList.add('is-loaded')
-    image.parentElement.style.backgroundImage = `url('${image.src}')`
-})
+const image = document.getElementsByTagName('img')[0]
+const length = 10
+const indices = [...new Array(length).keys()].map((index) => index + Math.random() * 0.5)
+
+function update() {
+  const time = performance.now() / 5000
+
+  image.style.clipPath = `polygon(${indices.map((index) => {
+    const angle = Math.PI * 2 / 10 * (index + time)
+    return `${Math.cos(angle) * 50 + 50}% ${Math.sin(angle) * 50 + 50}%`
+  }).join(', ')})`
+
+  requestAnimationFrame(update)
+}
+
+requestAnimationFrame(update)
